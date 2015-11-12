028937496e0fe0f8c6fb5dcac558175cfd81a8f4
Danko Adamczyk <danko@studyportals.com>
2015-11-12 11:41:35 +0100

    Initial commit with first set of functions.

---
+++ PowerShell/Directory.ps1
@@ -0,0 +1,12 @@
+# @file Directoy.ps1
+*
+* @author Danko Adamczyk <dankoadamczyk@me.com>
+* @version 1.0.0
+
+
+# Get the current folder.
+
+function getFolder(){
+
+    return Split-Path -leaf -path (Get-Location) + "\";
+}
