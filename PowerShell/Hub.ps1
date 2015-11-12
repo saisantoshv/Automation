bea131479ddf99050f7f0fb6178602584dd60f5c
Danko Adamczyk <danko@studyportals.com>
2015-11-12 12:25:43 +0100

    Automatically open the page on github if you want that.

--- PowerShell/Hub.ps1
+++ PowerShell/Hub.ps1
@@ -32,27 +32,30 @@ function ask{
 
 function pr{
 
-  if((ask('Do you want to create an Pull Request based on an existing issue?')) -eq 'y'){
+    if((ask('Do you want to create an Pull Request based on an existing issue?')) -eq 'y'){
 
-    hub issue;
+        hub issue;
 
-    $Issue = Read-Host -Prompt 'Please provide an issue number or press [s] to stop'
+        $Issue = Read-Host -Prompt 'Please provide an issue number or press [s] to stop'
 
-    if ($Issue -eq 's'){
+        if ($Issue -eq 's'){
 
-        RETURN
-    }
+            RETURN
+        }
 
-    if ($issue -gt 0){
+        if ($issue -gt 0){
         
-        hub pull-request -b develop -i $Issue
-        RETURN
+            hub pull-request -b develop -i $Issue
+        }
     }
-  }
+    elseif((ask('Do you want to create a new Pull Request?')) -eq 'y'){
 
-  if((ask('Do you want to create a new Pull Request?')) -eq 'y'){
+        hub pull-request -b develop
+    }
 
-    hub pull-request -b develop
-    RETURN
-  }
+    if((ask('Do you want to view the branch on GitHub??')) -eq 'y'){
+
+        # TODO: It would be great if we can directly access the PR instead of the branch.
+        hub browse
+    }
 }
