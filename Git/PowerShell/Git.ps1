bd25c0656a338186c34a79775cc55c7399658679
Danko Adamczyk <danko@studyportals.com>
2016-01-20 12:51:04 +0100

    Added functionality to checkout the repositories on a specific branch, pull it and prune all the already merged branches.

--- Git/PowerShell/Git.ps1
+++ Git/PowerShell/Git.ps1
@@ -21,7 +21,112 @@ function cb(){
         git co $BaseBranch
     }
 
-    git pull
+    git pull --ff-only
     git co -b $NewBranch
     git push --set-upstream origin $NewBranch
+}
+
+<#
+.SYNOPSIS
+
+Prune the branches and delete already merged branches.
+#>
+
+function prune{
+    
+    & git remote prune origin
+	& git branch --merged | ?{-not $_.trim().indexOf("*") -eq 0} | %{
+        
+        & git branch -d $_.trim()
+    }    
+}
+
+<#
+.SYNOPSIS
+Clean this repository and remove already merged branches.
+#>
+
+function Clean-Repo{
+
+	param(
+		$branch = 'develop'
+	)
+
+    & git status 2>&1 | Out-Null
+
+    if($LastExitCode -ne 0){
+    
+        Pop-Location
+        return
+    }
+
+    $Folder = (Get-Item -Path ".\" -Verbose).BaseName;
+
+    '{0}: Starting to clean on branch {1}' -f $Folder, $branch | Write-Host -ForegroundColor White
+
+        
+    & git fetch --all 2>&1 | Out-Null
+    if($LastExitCode -ne 0){
+
+        '{0}: unable to fetch. Investigate and re-run this script.' -f $Folder | Write-Host -ForegroundColor Red
+        Pop-Location
+        return
+    }
+
+    & git checkout $branch 2>&1 | Out-Null
+    if($LastExitCode -ne 0){
+
+        '{0}: unable to checkout {1}. Investigate and re-run this script.' -f $Folder, $branch | Write-Host -ForegroundColor Red
+        Pop-Location
+        return
+    }
+
+    $Status = (& git status)
+    $BranchStatus = $Status[1]
+
+    if($BranchStatus -like "Your branch is up-to-date with 'origin/$branch'."){
+
+        prune
+        '{0}: {1}' -f $Folder, $BranchStatus | Write-Host -ForegroundColor Green
+        Pop-Location
+        return
+    }
+
+    & git pull --ff-only 2>&1 | Out-Null
+    if($LastExitCode -ne 0){
+
+        '{0}: unable to pull {1}. Investigate and re-run this script.' -f $Folder, $branch | Write-Host -ForegroundColor Red
+        Pop-Location
+        return
+    }
+        
+    $Status = (& git status)
+    if($Status[2] -notlike 'nothing to commit, working directory clean' -and $Status[3] -notlike 'nothing to commit, working directory clean' ){
+
+        '{0}: Investigate and re-run this script.' -f $Status | Write-Host -ForegroundColor Red
+        Pop-Location
+        return
+    }
+
+    prune
+    '{0}: Done.' -f $Folder | Write-Host -ForegroundColor Green
+}
+
+<#
+.SYNOPSIS
+Iterate over all folders and call CleanRepo
+#>
+
+function Clean-Repos{
+
+	param(
+		$branch = 'develop'
+	)
+
+    Get-ChildItem . | %{
+
+        Push-Location $_.FullName;
+        Clean-Repo -branch $branch
+        Pop-Location
+    }
 }
